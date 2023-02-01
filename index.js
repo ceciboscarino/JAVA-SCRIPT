c4952a4a4c15b98711d263a228e638bd06357c91
ceciboscarino <ceciboscarino@hotmail.com>
2023-02-01 14:45:35 -0300

    iniciando js

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hola mundo")
+
+var numero =5;
+console.log(numero)
+
+var miVariable =5;
+console.log(miVariable)
+
+let miVariable2
+var op1 = 2;
+var op2 = 3;
+var resultado = op1 +op2;
+console.log("resultado vale:" + resultado)
